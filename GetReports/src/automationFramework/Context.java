bc1ea5e559f66e4fe85a404005d44abbdf4ab753
d553h471 <d553h471@ITS-8JFFNN1.home.ku.edu>
2017-06-19 11:34:11 -0500

    Cleaned up junk.

--- GetReports/src/automationFramework/Context.java
+++ GetReports/src/automationFramework/Context.java
@@ -1,9 +1,5 @@
 package automationFramework;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.WatchService;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,7 +10,6 @@ public class Context {
 	public String printerName;
 	public WebDriverWait waitShort;
 	public WebDriverWait waitLong;
-	boolean downloadComplete;
 	
 	public Context(WebDriver webDriver, WebDriverWait waitShort, WebDriverWait waitLong,String ipAddress, String pwString, String printerName){
 		this.ipAddress = ipAddress;
