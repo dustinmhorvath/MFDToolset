c6d561cf18255c9a73f6da32675e89e99bf41f28
Dustin Horvath <dustinmhorvath@gmail.com>
2017-07-05 12:03:19 -0500

    Added javadoc.

--- GetReports/src/automationFramework/ExcelReader.java
+++ GetReports/src/automationFramework/ExcelReader.java
@@ -16,10 +16,16 @@ public class ExcelReader {
 	private int rows;
 	private int cols;
 
-    public ExcelReader(String filenamearg) throws FileNotFoundException, IOException{
+	/**
+	 * Creates an object that can read Excel .xlsx filetypes. Can be used to read cells using row-col coordinates.
+	 * @param excelFilenameWithPath Absolutely path to the file to be opened, including the filename.
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+    public ExcelReader(String excelFilenameWithPath) throws FileNotFoundException, IOException{
         // An excel file name. You can create a file name with a full
         // path information.
-        filename = filenamearg;
+        filename = excelFilenameWithPath;
 
         try (FileInputStream fis = new FileInputStream(filename)) {
             // Create an excel workbook from the file system.
@@ -38,14 +44,27 @@ public class ExcelReader {
         //showExcelData(sheetData);
     }
     
-    
+    /**
+     * Return a string containing the contents of the cell at (row, col).
+     * @param row 0-indexed row of the cell you wish to retrieve.
+     * @param col 0-indexed column of the cell you wish to retrieve.
+     * @return String containing the contents of (row, col).
+     */
     public String getValueAt(int row, int col){
     	return sheet.getRow(row).getCell(col).toString();
     }
     
+    /**
+     * Get the number of rows in the Excel file.
+     * @return Number of rows read in.
+     */
     public int getRows(){
     	return rows;
     }
+    /**
+     * Get the number of columns in the Excel file.
+     * @return Number of columns read in.
+     */
     public int getCols(){
     	return cols;
     }
